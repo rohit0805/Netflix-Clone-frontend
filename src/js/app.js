f5b0b265721de8125f878562a3c48cf26116ff19
Rohit Negi <rohit5822540@gmail.com>
2020-07-08 20:26:26 +0530

    Added alert features of email input

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,48 @@
 import '../scss/style.scss';
 
 var Controller=(function(){
+    var flag=0,btn_element;
     var selector={
         placeholder:document.querySelector('.placeholder'),
-        input_email:document.querySelector('.input_email')
+        input_email:document.querySelector('.input_email'),
+        alert:document.querySelector('.alert_msz'),
     };
+
     var ChangePosition=(e)=>{
         selector.placeholder.classList.add('change_top');
     };
     var RemovePosition=(e)=>{
-        selector.placeholder.classList.remove('change_top');
-    }
+        if(selector.input_email.value==0)
+            selector.placeholder.classList.remove('change_top');
+        flag=1;
+        AlertMessage(e);
+    };
+    var AlertMessage=(e)=>{
+        if(flag){
+            selector.alert.style.display="block";
+            selector.input_email.style.borderBottomColor="#ffa00a";
+            var email=selector.input_email.value;
+            var check=email.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi);
+            if(check){
+                selector.alert.style.display="none";
+            }
+            else{
+                if(email.length>=5){
+                    selector.alert.innerHTML="Please enter a valid email address";
+                }
+                else{
+                    selector.alert.innerHTML="Email is required";
+                }
+            }
+        }
+    };
+
     var SetupEventListener=()=>{
         //event for placeholder
         selector.input_email.addEventListener('focus',ChangePosition);
-        selector.input_email.addEventListener('blur',RemovePosition);
+        selector.input_email.addEventListener('blur',RemovePosition);  
+        //alert event
+        selector.input_email.addEventListener('input',AlertMessage);
     };
     return {
         init:()=>{
